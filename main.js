14de0c20c96801890fbc796686f0b393abe52f7a
sk <sk@nimirum.info>
2017-02-24 13:36:50 +0100

    create hash for id

--- main.js
+++ main.js
@@ -8,16 +8,29 @@
 	inputSearch.focus();
 	var timestamp = new Date();
 
+	String.prototype.hashCode = function() {
+	  var hash = 0, i, chr, len;
+	  if (this.length === 0) return hash;
+	  for (i = 0, len = this.length; i < len; i++) {
+	    chr   = this.charCodeAt(i);
+	    hash  = ((hash << 5) - hash) + chr;
+	    hash |= 0; // Convert to 32bit integer
+	  }
+	  return hash;
+	};
+
 	$(".search__form").submit(function(event) {
 
 	    //stop form from submitting normally
 	    event.preventDefault();
     
 	    var queryString = $(this).find('input[name="search"]').val();
+
+	    var _id = queryString.hashCode();
 	        
 	    $.ajax({
 		  method: "PUT",
-		  url: "https://frag.nimirum.de/es/questions/question/remote-test",
+		  url: "https://frag.nimirum.de/es/questions/question/" + _id,
 		  contentType: "application/json",
 		  data: JSON.stringify({ 
 		  	"query-string": queryString,
